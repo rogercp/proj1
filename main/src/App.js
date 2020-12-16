93993199f70ca8bf75f08b2310455902a4350b7d
rogercp <rogercp1234@yahoo.com>
2020-12-15 23:41:45 -0800

    created landing

--- main/src/App.js
+++ main/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Landing from './views/Landing' 
 
 function App() {
   return (
@@ -7,14 +8,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Landing/>
       </header>
     </div>
   );
---
+++ main/src/views/Landing.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Landing() {
+  return (
+    <div className="landingPage">
+     <h1>landing page</h1>
+    </div>
+  );
+}
+
+export default Landing;
